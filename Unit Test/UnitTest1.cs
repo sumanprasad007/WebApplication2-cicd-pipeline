6f3ae236d1402fccb2438cc64a7bd4fc3a7c918d
Suman Prasad <sumanprasad9766@gmail.com>
2023-05-01 13:32:00 +0530

    added the unit test to the repository

---
+++ Unit Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
